38c089666300695c02854f88b9948e3d1a438b4d
arthurhtml <arthur.dbs70@gmail.com>
2021-04-01 20:35:59 +0200

    20h30

--- src/Window/Fenetre.java
+++ src/Window/Fenetre.java
@@ -1,9 +1,17 @@
 package Window;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class Fenetre extends Stage {
+import java.awt.*;
 
+import static javafx.application.Application.launch;
+
+public class Fenetre extends Stage {
 
 
 
